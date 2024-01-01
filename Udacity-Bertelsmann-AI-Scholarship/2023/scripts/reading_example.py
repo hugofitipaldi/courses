ac8d25877df71171306e19837b0feda2697b41b4
Hugo Fitipaldi <33068886+hugofitipaldi@users.noreply.github.com>
2024-01-01 14:33:30 -0300

    working on udacity

---
+++ Udacity-Bertelsmann-AI-Scholarship/2023/scripts/reading_example.py
@@ -0,0 +1,6 @@
+# Reading and Writing files
+
+f = open('../data/my_file.txt', 'r') 
+file_data = f.read()
+print(file_data)
+f.close()
---
+++ Udacity-Bertelsmann-AI-Scholarship/2023/scripts/writing_example.py
@@ -0,0 +1,5 @@
+# Writing to a file
+
+f = open("../data/my_file.txt", "w")
+f.write("Hello There!\n")
+f.close()
